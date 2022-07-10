980012dafab2957d8e19dfaf0798628b0ba8ca5f
victorhugosd <52414875+victorhugosd@users.noreply.github.com>
2022-07-10 12:34:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ pngdata = f.asDirect()
 image_2d = np.vstack(map(np.uint16, pngdata[2]))
 #w.write(f, s)
 
-image_2d = np.transpose(image_2d)
+image_2d = image_2d / 256
 
-plt.imsave('test2.png', (image_2d), cmap=cm.gray)
+u,s,v = np.linalg.svd(image_2d)
+
+k = 200
+
+u = u[:,0:k]
+s = np.diag(s[0:k])
+v = v[0:k,:]
+
+image_2d = 256* u @ s @ v
+
+plt.imsave('gato2.png', (image_2d), cmap=cm.gray)
