d96196d081bbf2c77dc9a264faf671c57149b67c
Sofia <sofia98@usp.br>
2022-07-12 14:02:55 -0300

    comentarios

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
-from numpy.linalg import norm
 from math import pi, cos, sin, sqrt
 import png
 from random import normalvariate
@@ -10,18 +9,19 @@ f = png.Reader('gato.png')
 pngdata = f.asDirect()
 
 # matriz cujas entradas correspondem ao tom de cinza de cada pixel da imagem
-image_2d = np.vstack(map(np.uint16, pngdata[2]))
+image_2d = np.vstack(list(pngdata[2]))
 
 
 # vetor unitario aleatorio com distribuicao uniforme
+# gerado usando uma distribuicao uniforme em [0,1]^n e normalizando
 def vetor_aleatorio(n):
-    unnormalized = [normalvariate(0, 1) for _ in range(n)]
-    theNorm = sqrt(sum(x * x for x in unnormalized))
-    return [x / theNorm for x in unnormalized]
+    vec = [normalvariate(0, 1) for _ in range(n)]
+    norma = sqrt(sum(x * x for x in vec))
+    return [x / norma for x in vec]
 
 
 # decomposicao SVD unidimensional
-def svd_1d(A, tol=1e-10):
+def svd_1d(A, tol):
 
     n, m = A.shape
     x = vetor_aleatorio(min(n,m))
@@ -38,7 +38,7 @@ def svd_1d(A, tol=1e-10):
         iterations += 1
         lastV = currentV
         currentV = np.dot(B, lastV)
-        currentV = currentV / norm(currentV)
+        currentV = currentV / np.linalg.norm(currentV)
 
         if abs(np.dot(currentV, lastV)) > 1 - tol:
             print("converged in {} iterations!".format(iterations))
@@ -47,29 +47,27 @@ def svd_1d(A, tol=1e-10):
 
 # decomposicao SVD da matriz A
 # calculada usando metodo das potencias
-# k eh o numero de valores singulares a serem computados
-def svd(A, k=None, tol=1e-10):
+# o segundo argumento eh o numero de valores singulares a serem computados
+def svd(A, k, tol=1e-10):
     A = np.array(A, dtype=float)
     n, m = A.shape
     svdSoFar = []
-    if k is None:
-        k = min(n, m)
 
     for i in range(k):
-        matrixFor1D = A.copy()
+        matriz_1d = A.copy()
 
-        for singularValue, u, v in svdSoFar[:i]:
-            matrixFor1D -= singularValue * np.outer(u, v)
+        for valor_singular, u, v in svdSoFar[:i]:
+            matriz_1d -= valor_singular * np.outer(u, v)
 
         if n > m:
-            v = svd_1d(matrixFor1D, tol=tol)  # next singular vector
+            v = svd_1d(matriz_1d, tol)  # proximo vetor singular
             u_unnormalized = np.dot(A, v)
-            sigma = norm(u_unnormalized)  # next singular value
+            sigma = np.linalg.norm(u_unnormalized)  # proximo valor singular
             u = u_unnormalized / sigma
         else:
-            u = svd_1d(matrixFor1D, tol=tol)  # next singular vector
+            u = svd_1d(matriz_1d, tol) 
             v_unnormalized = np.dot(A.T, u)
-            sigma = norm(v_unnormalized)  # next singular value
+            sigma = np.linalg.norm(v_unnormalized)
             v = v_unnormalized / sigma
 
         svdSoFar.append((sigma, u, v))
@@ -77,7 +75,7 @@ def svd(A, k=None, tol=1e-10):
     singularValues, us, vs = [np.array(x) for x in zip(*svdSoFar)]
     return singularValues, us.T, vs
 
-p,q,r=svd(image_2d,k=20)
+p,q,r=svd(image_2d, 20)
 
 A = q@np.diag(p)@r
 
