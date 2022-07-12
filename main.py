b6ead11e4ef6ab3972c71cd9c7f4c88f644b2d01
Sofia <sofia98@usp.br>
2022-07-12 13:47:36 -0300

    comentarios

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ from random import normalvariate
 
 f = png.Reader('gato.png')
 pngdata = f.asDirect()
+
+# matriz cujas entradas correspondem ao tom de cinza de cada pixel da imagem
 image_2d = np.vstack(map(np.uint16, pngdata[2]))
-#w.write(f, s)
 
-#image_2d = np.transpose(image_2d)
 
-def randomUnitVector(n):
+# vetor unitario aleatorio com distribuicao uniforme
+def vetor_aleatorio(n):
     unnormalized = [normalvariate(0, 1) for _ in range(n)]
     theNorm = sqrt(sum(x * x for x in unnormalized))
     return [x / theNorm for x in unnormalized]
 
 
-def svd_1d(A, epsilon=1e-10):
-    ''' The one-dimensional SVD '''
+# decomposicao SVD unidimensional
+def svd_1d(A, tol=1e-10):
 
     n, m = A.shape
-    x = randomUnitVector(min(n,m))
+    x = vetor_aleatorio(min(n,m))
     lastV = None
     currentV = x
 
@@ -39,37 +40,34 @@ def svd_1d(A, epsilon=1e-10):
         currentV = np.dot(B, lastV)
         currentV = currentV / norm(currentV)
 
-        if abs(np.dot(currentV, lastV)) > 1 - epsilon:
+        if abs(np.dot(currentV, lastV)) > 1 - tol:
             print("converged in {} iterations!".format(iterations))
             return currentV
 
 
-def svd(A, k=None, epsilon=1e-10):
-    '''
-        Compute the singular value decomposition of a matrix A
-        using the power method. A is the input matrix, and k
-        is the number of singular values you wish to compute.
-        If k is None, this computes the full-rank decomposition.
-    '''
+# decomposicao SVD da matriz A
+# calculada usando metodo das potencias
+# k eh o numero de valores singulares a serem computados
+def svd(A, k=None, tol=1e-10):
     A = np.array(A, dtype=float)
     n, m = A.shape
     svdSoFar = []
     if k is None:
         k = min(n, m)
 
-    for _ in range(k):
+    for i in range(k):
         matrixFor1D = A.copy()
 
         for singularValue, u, v in svdSoFar[:i]:
             matrixFor1D -= singularValue * np.outer(u, v)
 
         if n > m:
-            v = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
+            v = svd_1d(matrixFor1D, tol=tol)  # next singular vector
             u_unnormalized = np.dot(A, v)
             sigma = norm(u_unnormalized)  # next singular value
             u = u_unnormalized / sigma
         else:
-            u = svd_1d(matrixFor1D, epsilon=epsilon)  # next singular vector
+            u = svd_1d(matrixFor1D, tol=tol)  # next singular vector
             v_unnormalized = np.dot(A.T, u)
             sigma = norm(v_unnormalized)  # next singular value
             v = v_unnormalized / sigma
